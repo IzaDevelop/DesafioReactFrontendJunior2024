c1323863fd16a217245105d783e006ac07d9c4cf
Izabelle <izabelledevelop@gmail.com>
2024-06-24 13:31:09 -0300

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -2,4 +2,4 @@ import ReactDOM from 'react-dom/client'
 import App from './app.tsx'
 import "./styles/global.css"
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
